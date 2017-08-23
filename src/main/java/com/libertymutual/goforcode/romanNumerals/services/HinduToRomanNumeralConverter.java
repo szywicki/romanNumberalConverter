20cc65bf9a15213b5b1019e680f061c86f6e22a3
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-08-22 19:01:06 -0500

    update to converter controller

--- src/main/java/com/libertymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/libertymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -6,81 +6,81 @@ public class HinduToRomanNumeralConverter {
         String roman="";
         int repeat;
  
-        repeat=numberToCovert/1000;
+        repeat = numberToCovert/1000;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"M";
+            roman = roman+"M";
         }
-        numberToCovert=numberToCovert%1000;
+        numberToCovert = numberToCovert%1000;
  
-        repeat=numberToCovert/900;
+        repeat = numberToCovert/900;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"CM";
+            roman = roman+"CM";
         }
-        numberToCovert=numberToCovert%900;
+        numberToCovert = numberToCovert%900;
  
-        repeat=numberToCovert/500;
+        repeat = numberToCovert/500;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"D";
+            roman = roman+"D";
         }
         numberToCovert=numberToCovert%500;
  
-        repeat=numberToCovert/400;
+        repeat = numberToCovert/400;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"CD";
+            roman = roman+"CD";
         }
-        numberToCovert=numberToCovert%400;
+        numberToCovert = numberToCovert%400;
  
-        repeat=numberToCovert/100;
+        repeat = numberToCovert/100;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"C";
+            roman = roman+"C";
         }
-        numberToCovert=numberToCovert%100;
+        numberToCovert = numberToCovert%100;
  
-        repeat=numberToCovert/90;
+        repeat = numberToCovert/90;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"XC";
+            roman = roman+"XC";
         }
-        numberToCovert=numberToCovert%90;
+        numberToCovert = numberToCovert%90;
  
-        repeat=numberToCovert/50;
+        repeat = numberToCovert/50;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"L";
+            roman = roman+"L";
         }
-        numberToCovert=numberToCovert%50;
+        numberToCovert = numberToCovert%50;
  
-        repeat=numberToCovert/40;
+        repeat = numberToCovert/40;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"XL";
+            roman = roman+"XL";
         }
-        numberToCovert=numberToCovert%40;
+        numberToCovert = numberToCovert%40;
  
-        repeat=numberToCovert/10;
+        repeat = numberToCovert/10;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"X";
+            roman = roman+"X";
         }
-        numberToCovert=numberToCovert%10;
+        numberToCovert = numberToCovert%10;
  
-        repeat=numberToCovert/9;
+        repeat = numberToCovert/9;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"IX";
+            roman = roman+"IX";
         }
-        numberToCovert=numberToCovert%9;
+        numberToCovert = numberToCovert%9;
  
-        repeat=numberToCovert/5;
+        repeat = numberToCovert/5;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"V";
+            roman = roman+"V";
         }
-        numberToCovert=numberToCovert%5;
+        numberToCovert = numberToCovert%5;
  
-        repeat=numberToCovert/4; 
+        repeat = numberToCovert/4; 
         for(int i=1; i<=repeat;i++){
-            roman=roman+"IV";
+            roman = roman+"IV";
         }
-        numberToCovert=numberToCovert%4;
+        numberToCovert = numberToCovert%4;
  
-        repeat=numberToCovert/1; // or simply repeat=n or i<=n in the condition part of the loop
+        repeat = numberToCovert;
         for(int i=1; i<=repeat;i++){
-            roman=roman+"I";
+            roman = roman+"I";
         }
         return roman;
     }
